64d69d677fdc7339cbbbc026ed7e2d38fdbe71ce
vincent-guizot <vincentguizot128@gmail.com>
2024-10-24 19:51:15 +0700

    npm intro done

---
+++ npm_intro/app.js
@@ -0,0 +1,7 @@
+// const bootstrap = require('bootstrap');
+
+// const tailwind = require('tailwindcss');
+
+console.log("tes");
+console.log("belajar npm");
+console.log("fungsi dari nodemon adalah run app secara automatic");
